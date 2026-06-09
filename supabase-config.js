1d1396fdd10cdfcc746897eee8bfe0d99575672d
gabrielhsouza1999-cell <gabrielhsouza1999@gmail.com>
2026-06-09 14:48:17 -0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.GH_SUPABASE = {
-  url: "https://xmmaqquwslparirykgvp.supabase.co",
+  url: "https://xmmaqguwslparirykgvp.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtbWFxZ3V3c2xwYXJpcnlrZ3ZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMTU4OTUsImV4cCI6MjA5NjU5MTg5NX0.X1luRnf8iD2OimUtHPyHQctryfBUpZH7acipF9CS7Gc",
 };
